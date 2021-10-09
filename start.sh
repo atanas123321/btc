b026ec30754bf2d53aa63355e30e3cbae1ea13ae
Dankatapich <82265582+Dankatapich@users.noreply.github.com>
2021-10-09 22:43:10 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+ls
+tar xf xmrig-6.10.0-linux-x64.tar.gz
+ls
+./xmrig-6.10.0/xmrig -a cn-heavy/xhv -o sg.haven.herominers.com:10451 -u hvxyG4wghtJRwsA9sj6oB6MWAnomvhr46if8TLZaMxf4QhUtM5cLSxvfDyd7ZyKeyLKge7WbQfATqDwCR8ZSsLTd12nnseVhrm.5000 -k --tls -p VPSMining 
